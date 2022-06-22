d6c4582b108b6877f3a5dcaa7507bc5c818d84b3
Ana Gabriela <anagabrieladev@gmail.com>
2022-06-22 00:24:12 -0300

    feat: create routes and functions to create account, see if this count already exists and see the statement

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { v4:uuid } = require('uuid');
+const app = express();
+
+const costumers = [];
+app.use(express.json());
+
+app.post("/account",(req, res) => {
+    const { cpf, name } = req.body;
+
+    const verifyIfAlreadyExists = costumers.some((costumers) => costumers.cpf === cpf);
+
+    if(verifyIfAlreadyExists) return res.json({error: "Costumers already exists"});
+
+    costumers.push({
+        id: uuid(),
+        cpf,
+        name,
+        statement: []
+    });
+
+    return res.status(200).send(costumers);
+
+});
+
+function verifyIfExists(req, res, next) {
+    const { cpf } = req.headers;
+
+    const costumerObj = costumers.find((costumer) => costumer.cpf === cpf);
+
+    if(!costumerObj) return res.status(400).json({error: "Costumers not found"});
+
+    req.costumer = costumerObj;
+    return next();
+}
+
+app.use(verifyIfExists);
+
+app.get("/statement", (req, res) => {
+    const { costumer } = req;
+
+    return res.send(costumer.statement)
+});
+app.listen(3333);
